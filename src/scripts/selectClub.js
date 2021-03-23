68596f0dae49a89015cff47c0024db1f1fcf8e10
Nina Zurbulaeva <naina148@mail.ru>
2021-03-24 01:15:12 +0300

    feat: rewrite function selectClub | scripts/

--- src/scripts/selectClub.js
+++ src/scripts/selectClub.js
@@ -1,9 +1,18 @@
 export default function selectClub() {
-    const clubSelect = document.querySelector('.club-select');
-    console.log('selectClub');
-    clubSelect.addEventListener('click', event => {
-        clubSelect.children[0].getElementsByClassName.display = 'fixed';
-        console.log('click', event);
-    });
+    const clubList = document.querySelector('.clubs-list');
+    clubList.addEventListener('click', event => {
+        
+        [...clubList.children].forEach(elem => {
+            // console.log('elem ', elem);
+            if (elem.nodeName === 'UL'){
+                if (elem.classList.contains('open')) {
+                    elem.classList.toggle('open');
+                    elem.style.display = 'none';
+                } else {
+                    elem.style.display = 'block';
+                    elem.classList.add('open');
+                }
+            }   
+        })
+    })       
 }
-
