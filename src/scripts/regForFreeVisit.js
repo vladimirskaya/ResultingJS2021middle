47a2c5d273efded5daeeebf9cd8d038d06ebf1df
Nina Zurbulaeva <naina148@mail.ru>
2021-03-24 10:11:18 +0300

    feat: add extra condition for closing modalwindow | scripts/

--- src/scripts/regForFreeVisit.js
+++ src/scripts/regForFreeVisit.js
@@ -2,13 +2,20 @@ export default function regForFreeVisit() {
     
     const freeVisitForm = document.querySelector('#free_visit_form'),
         openPopup = document.querySelector('.open-popup'),
+        formContent = document.querySelectorAll('.form-content')[1],
         doc = document.querySelector('body');
 
         //док  - для дальнейшего создания единого слушателя для всех событий
         doc.addEventListener('click', event => {
             let target = event.target;
             if (target === openPopup) freeVisitForm.style.display = 'block';
-            if (event.target.nodeName === "IMG") freeVisitForm.style.display = 'none';
+            // console.log('Элемент, в котором находитс таргет', formContent);
+            // console.dir('Его дети', formContent.children);
+            // console.log('Сам таргет', target);
+            // console.log('Таргет внутри главного элемента', formContent.contains(target));
+            if (target.nodeName === "IMG" || !formContent.contains(target) && target !== openPopup) {
+                freeVisitForm.style.display = 'none';
+            } 
             
     })       
 }
