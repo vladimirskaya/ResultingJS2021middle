6b890a1a2e2403853f208299c1b74ef3de0fbac2
Nina Zurbulaeva <naina148@mail.ru>
2021-03-26 21:08:17 +0300

    feat:rewrite code for sendForm.js | modules

--- src/modules/burgerMenuPopup.js
+++ src/modules/burgerMenuPopup.js
@@ -2,21 +2,16 @@ export default function burgerMenuPopup() {
     const popupMenuHidLarge = document.querySelector('.popup-menu'),
         btnClosePopupMenu = document.querySelector('.close-menu-btn'),
         btnPopupMenu = document.querySelector('.menu-button');
-        console.log(btnPopupMenu);
-
-
+    
         btnPopupMenu.addEventListener('click', event =>{
-            let target = event.target;
-           
-            console.log(target);
+            let target = event.target;                      
             if (target === btnPopupMenu.children[0]) {
                 popupMenuHidLarge.style.display = 'flex';
             }
         });
 
         popupMenuHidLarge.addEventListener('click', event =>{
-            let target = event.target;
-            
+            let target = event.target;          
             if (target === btnClosePopupMenu.children[0]){
                 popupMenuHidLarge.style.display = 'none';
             } else{
